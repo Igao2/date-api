5f86d7085c2fba9af2bb171dcf6eac8b03ebc30d
Igao2 <igor.dasilvacosta8@gmail.com>
2024-08-19 21:28:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const moment = require('moment'); 
+const port = process.env.PORT || 8000;
+app.use(express.static('www'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function calculardif(datai,dataf,format){
+    const diffMs = Math.abs(new Date(dataf) - new Date(datai));
+    const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+    const diffMonths = Math.floor(diffDays/30);
+    const diffYears = Math.floor(diffDays/365);
+    const diffHours = Math.floor((diffMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const diffMinutes = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
+    let result = {};
+    switch (format) {
+      case 'YMdhm':
+        result = {
+          years: diffYears,
+          months: diffMonths,
+          days: diffDays,
+          hours: diffHours,
+          minutes: diffMinutes
+        };
+        break;
+      case 'YMd':
+        result = { years: diffYears, months: diffMonths, days: diffDays };
+        break;
+      case 'Y':
+        result = { years: diffYears };
+        break;
+      case 'M':
+        result = { months: diffMonths };
+        break;
+      case 'd':
+        result = { days: diffDays };
+        break;
+      case 'h':
+        result = { hours: diffHours };
+        break;
+      case 'm':
+        result = { minutes: diffMinutes };
+        break;
+      case 'dhm':
+        result = { days: diffDays, hours: diffHours, minutes: diffMinutes };
+        break;
+      case 'dh':
+        result = { days: diffDays, hours: diffHours };
+        break;
+      case 'dm':
+        result = { days: diffDays, minutes: diffMinutes };
+        break;
+      case 'hm':
+        result = { hours: diffHours, minutes: diffMinutes };
+        break;
+      default:
+        result = { error: 'Formato não reconhecido' };
+    }
+  
+    return result;
+  
+    
+}
+app.get("/diferencadatas",(req,res)=>{
+    const {datai,h1,dataf,h2,format="dhm"} = req.query;
+    const dateFormat = 'DD/MM/YYYY';
+    const timeFormat = 'HH:mm';
+  
+    const dateTime1 = moment(`${datai} ${h1}`, `${dateFormat} ${timeFormat}`);
+    const dateTime2 = moment(`${dataf} ${h2}`, `${dateFormat} ${timeFormat}`);
+    if (!datai || !dataf) {
+        return res.status(400).send('Parâmetros date1 e date2 são obrigatórios.');
+      }
+    if (!dateTime1.isValid() || !dateTime2.isValid()) {
+            return res.status(400).json({ error: 'Formato de data ou hora inválido. Use DD/MM/YYYY para a data e HH:mm para a hora.' });
+     }
+        
+    var result = calculardif(dateTime1,dateTime2,format)
+    res.json({ result })
+    })
+app.listen(port,()=>{
+    console.log("AAA")
+})
